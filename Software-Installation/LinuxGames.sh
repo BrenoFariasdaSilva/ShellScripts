d6515089810d06ebdd90a339dcbfd47de0c80735
Breno Farias <57010178+BrenoFariasdaSilva@users.noreply.github.com>
2023-07-25 15:12:52 -0300

    FEAT: Adding My LinuxGames ShellScript

---
+++ Software-Installation/LinuxGames.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+# Run:
+# chmod +x LinuxGames.sh
+# sudo ./LinuxGames.sh
+
+# Open terminal as root:
+# sudo -i
+sudo su
+
+# Update commands:
+sudo apt update -y
+
+#---Games Softwares---#
+
+# Steam:
+sudo add-apt-repository multiverse -y
+sudo apt update -y
+sudo apt install steam -y
+
+# Update System:
+sudo apt update -y
