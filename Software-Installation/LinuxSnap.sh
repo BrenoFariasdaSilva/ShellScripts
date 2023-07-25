08db57f5be0e1437f6b11c2e3757ae1a2686a264
Breno Farias <57010178+BrenoFariasdaSilva@users.noreply.github.com>
2023-07-25 15:13:07 -0300

    FEAT: Adding My LinuxSnap ShellScript

---
+++ Software-Installation/LinuxSnap.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+
+# Run:
+# chmod +x LinuxSnap.sh
+# sudo ./LinuxSnap.sh
+
+# Open terminal as root:
+# sudo -i
+sudo su
+
+# Update commands:
+sudo apt update -y
+
+#---Snap Softwares---#
+
+# Snapd:
+sudo apt install snapd -y
+sudo snap install core -y
+sudo snap install snap-store
+
+# Snap Softwares:
+sudo snap install authy
+sudo snap install discographer
+sudo snap install fast
+sudo snap install gnome-twitch
+sudo snap install mojave-themes
+sudo snap install netflix-web
+sudo snap install notepad-plus-plus
+sudo snap install notion-snap
+sudo snap install oxygen-cursors
+sudo snap install postman
+sudo snap install sc-controller
+sudo snap install spotify
+sudo snap install teams-for-linux
+sudo snap install whatsapp-for-linux
+sudo snap install yuzu
+
+# Update System:
+sudo apt update -y
