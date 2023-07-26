00670d35eb2d634757d98cb963f60895c341c7aa
Breno Farias <57010178+BrenoFariasdaSilva@users.noreply.github.com>
2023-07-26 20:05:21 -0300

    REFACTOR: Updating KeyboardShortcuts.sh

--- Software-Settings/KeyboardShortcuts.sh
+++ Software-Settings/KeyboardShortcuts.sh
@@ -6,19 +6,19 @@
 
 # Open Keyboard Shortcuts in System Preferences: Settings -> Keyboard Shortcuts
 
-# 1. Set "Show Applications" to "Super/Windows + S"
+# 1. Set the "Show Applications" shortcut to "Super/Windows + S"
 gsettings set org.gnome.shell.keybindings toggle-application-view "['<Super>s']"
 
-# 2. Set "Show Desktop" to "Super/Windows + D"
+# 2. Set the "Show Desktop" shortcut to "Super/Windows + D"
 gsettings set org.gnome.desktop.wm.keybindings show-desktop "['<Super>d']"s
 
-# 3. Change the "Home Folder" shortcut to "Super/Windows + E"
+# 3. Set the "Home Folder" shortcut to "Super/Windows + E"
 gsettings set org.gnome.settings-daemon.plugins.media-keys home "['<Super>e']"
 
-# 4. Change the "Terminal" shortcut to "Super/Windows + T"
+# 4. Set the "Terminal" shortcut to "Super/Windows + T"
 gsettings set org.gnome.settings-daemon.plugins.media-keys terminal "['<Super>t']"
 
-# 5. Change the "Close Window" shortcut to "Alt + F4"
+# 5. Set the "Close Window" shortcut to "Alt + F4"
 gsettings set org.gnome.desktop.wm.keybindings close "['<Alt>F4']"
 
 # If any of this doesn't work, add the failed ones manually:
