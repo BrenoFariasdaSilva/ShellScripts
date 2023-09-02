104a1dea07283245810f0d473eb9cd0d4c5adc2f
Breno Farias <57010178+BrenoFariasdaSilva@users.noreply.github.com>
2023-09-02 09:43:17 -0300

    REFACTOR: Updating Permissions ShellScripts

--- GrantPermissions.sh
+++ GrantPermissions.sh
@@ -6,7 +6,6 @@
 
 # Grant ShellScripts Execution Permissions
 grant_permissions() {
-  chmod +x GitHub/*.sh # Grant Execution Permission to all ShellScripts
   chmod +x Software-Installation/*.sh # Grant Execution Permission to all ShellScripts
   chmod +x Software-Installation/SubScripts/*.sh # Grant Execution Permission to all ShellScripts inside SubScripts folder
   chmod +x Software-Settings/*.sh # Grant Execution Permission to all ShellScripts
