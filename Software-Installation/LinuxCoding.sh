555d76c76b0b82aaf524258d16ebf1c9c1bb77f4
Breno Farias da Silva <breno.vicio78@gmail.com>
2024-02-10 16:34:04 -0300

    REFACTOR: Improving the Python Script in LinuxCoding.sh

--- Software-Installation/LinuxCoding.sh
+++ Software-Installation/LinuxCoding.sh
@@ -173,6 +173,7 @@ echo ""
 echo "Installing Python..."
 sudo apt install python3 python3-venv python3-pip -y
 sudo apt install python-is-python3
+python3 --version
 echo "Python Installed!"
 echo ""
 
