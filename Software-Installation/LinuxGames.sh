4689239f9530c7127e5f4111f4869ea8bd6f950e
Breno Farias da Silva <breno.vicio78@gmail.com>
2024-02-10 17:04:33 -0300

    REFACTOR: Adding Echo Messages in LinuxGames.sh

--- Software-Installation/LinuxGames.sh
+++ Software-Installation/LinuxGames.sh
@@ -5,11 +5,19 @@
 # sudo ./LinuxGames.sh
 
 # Update Commands:
+echo "Updating System..."
 sudo apt update -y
+echo "System Updated!"
+echo ""
 
 #---Games Softwares---#
 
 # Steam:
+echo "Installing Steam..."
 sudo add-apt-repository multiverse -y
 sudo apt update -y
 sudo apt install steam -y
+echo "Steam Installed!"
+
+echo "Games Softwares Installed!"
+echo ""
