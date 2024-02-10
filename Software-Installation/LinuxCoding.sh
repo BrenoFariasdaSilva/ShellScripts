5d2374c309e5eac1723837a79a521e3beaec3adc
Breno Farias da Silva <breno.vicio78@gmail.com>
2024-02-10 16:53:43 -0300

    REFACTOR: Improving the C# Script in LinuxCoding.sh

--- Software-Installation/LinuxCoding.sh
+++ Software-Installation/LinuxCoding.sh
@@ -72,7 +72,7 @@ wget https://packages.microsoft.com/config/ubuntu/22.04/prod.list
 sudo mv prod.list /etc/apt/sources.list.d/microsoft-prod.list
 sudo chown root:root /etc/apt/trusted.gpg.d/microsoft.asc.gpg
 sudo chown root:root /etc/apt/sources.list.d/microsoft-prod.list
-sudo apt update
+sudo apt update -y
 sudo apt install -y apt-transport-https &&
    sudo apt update &&
    sudo apt install -y dotnet-sdk-6.0
