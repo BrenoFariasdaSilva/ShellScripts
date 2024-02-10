681e2fa86a354c0bc513a10d4a1cdc84a2ae26b4
Breno Farias da Silva <breno.vicio78@gmail.com>
2024-02-10 17:02:05 -0300

    REFACTOR: Improving the PgAdmin Script in LinuxCoding.sh

--- Software-Installation/LinuxCoding.sh
+++ Software-Installation/LinuxCoding.sh
@@ -257,9 +257,8 @@ else
 fi
 
 sudo sh -c 'echo "deb https://ftp.postgresql.org/pub/pgadmin/pgadmin4/apt/$(lsb_release -cs) pgadmin4 main" > /etc/apt/sources.list.d/pgadmin4.list'
-sudo apt update -y
-sudo apt install pgadmin4-desktop -y
-sudo apt install pgadmin4-web -y
+curl https://www.pgadmin.org/static/packages_pgadmin_org.pub | sudo apt-key add
+sudo sh -c 'echo "deb https://ftp.postgresql.org/pub/pgadmin/pgadmin4/apt/$(lsb_release -cs) pgadmin4 main" > /etc/apt/sources.list.d/pgadmin4.list && apt update'
 sudo apt install pgadmin4 -y
 echo "PgAdmin Installed!"
 echo ""
