6a469acca96ee4938769d27b13b557767eaaa7da
Breno Farias da Silva <breno.vicio78@gmail.com>
2024-02-11 08:53:04 -0300

    REFACTOR: Improving the RunAll ShellScript Comments

--- Software-Installation/RunAll.sh
+++ Software-Installation/RunAll.sh
@@ -4,7 +4,15 @@
 # chmod +x RunAll.sh
 # sudo ./RunAll.sh
 
-# Update system
+# Update the package list
+update_package_list() {
+   echo "Updating System Package List..."
+   sudo apt update -y # Update package list
+   echo "System Package List Updated!"
+   echo ""
+}
+
+# Upgrade the system
 full_system_update() {
    echo "Upgrading System..."
    sudo apt dist-upgrade -y # Install updates
@@ -12,18 +20,10 @@ full_system_update() {
    echo "System Fully Upgraded!"
 }
 
-# Update package list
-update_package_list() {
-   echo "Updating System Package List..."
-   sudo apt update -y
-   echo "System Package List Updated!"
-   echo ""
-}
-
 # Grant ShellScripts Execution Permissions
 grant_permissions() {
    echo "Granting Execution Permissions..."
-   chmod +x ./*.sh          # Grant Execution Permission to all ShellScripts
+   chmod +x ./*.sh # Grant Execution Permission to all ShellScripts
    chmod +x SubScripts/*.sh # Grant Execution Permission to all ShellScripts inside SubScripts folder
    echo "Execution Permissions Granted!"
    echo ""
@@ -33,45 +33,53 @@ grant_permissions() {
 run_scripts() {
    echo "Running ShellScripts..."
    echo "Running the LinuxBasic ShellScript..."
-   ./LinuxBasic.sh  # Run LinuxBasic.sh
-   echo "LinuxBasic ShellScript Run!"
+   ./LinuxBasic.sh # Run LinuxBasic.sh
+   echo "LinuxBasic ShellScript Executed!"
    echo ""
 
    echo "Running the LinuxMedia ShellScript..."
-   ./LinuxMedia.sh  # Run LinuxMedia.sh
-   echo "LinuxMedia ShellScript Run!"
+   ./LinuxMedia.sh # Run LinuxMedia.sh
+   echo "LinuxMedia ShellScript Executed!"
    echo ""
 
    echo "Running the LinuxTools ShellScript..."
-   ./LinuxTools.sh  # Run LinuxTools.sh
-   echo "LinuxTools ShellScript Run!"
+   ./LinuxTools.sh # Run LinuxTools.sh
+   echo "LinuxTools ShellScript Executed!"
    echo ""
 
    echo "Running the LinuxCoding ShellScript..."
    ./LinuxCoding.sh # Run LinuxCoding.sh
-   echo "LinuxCoding ShellScript Run!"
+   echo "LinuxCoding ShellScript Executed!"
    echo ""
 
    echo "Running the LinuxGames ShellScript..."
-   ./LinuxGames.sh  # Run LinuxGames.sh
-   echo "LinuxGames ShellScript Run!"
+   ./LinuxGames.sh # Run LinuxGames.sh
+   echo "LinuxGames ShellScript Executed!"
    echo ""
 
    echo "Running the LinuxSnap ShellScript..."
-   ./LinuxSnap.sh   # Run LinuxSnap.sh
-   echo "LinuxSnap ShellScript Run!"
+   ./LinuxSnap.sh # Run LinuxSnap.sh
+   echo "LinuxSnap ShellScript Executed!"
    echo ""
 
-   echo "ShellScripts Run!"
+   echo "ShellScripts Executed!"
    echo ""
 }
 
 # Run all ShellScript SubScripts
 run_sub_scripts() {
    echo "Running SubScripts..."
-   ./SubScripts/BetterDiscord.sh   # Run BetterDiscord.sh
+
+   echo "Running the BetterDiscord SubScript..."
+   python3 ./SubScripts/BetterDiscord.py # Run BetterDiscord.py
+
+   echo "Running the GnomeExtensions SubScript..."
    ./SubScripts/GnomeExtensions.sh # Run LinuxAndroidStudio.sh
-   ./SubScripts/LinuxAnaconda.sh   # Run LinuxAnaconda.sh
+
+   echo "Running the LinuxAnaconda SubScript..."
+   ./SubScripts/LinuxAnaconda.sh # Run LinuxAnaconda.sh
+
+   echo "SubScripts Executed!"
 }
 
 # Run all functions
@@ -82,4 +90,4 @@ grant_permissions # Call the grant_permissions function to grant execution permi
 run_scripts # Call the run_scripts function to run all ShellScripts
 run_sub_scripts # Call the run_sub_scripts function to run all SubScripts
 update_package_list # Call the update_package_list function to update the package list
-echo "System Updated and ShellScripts Run!"
+echo "System Updated and ShellScripts Executed!"
