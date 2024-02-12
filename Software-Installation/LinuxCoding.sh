615cb62ae68fd9555b9c6f79395c757ca150c777
Breno Farias da Silva <breno.vicio78@gmail.com>
2024-02-12 16:03:56 -0300

    REFACTOR: Adding Ruby Version Manager to the Ruby Installation Script

--- Software-Installation/LinuxCoding.sh
+++ Software-Installation/LinuxCoding.sh
@@ -293,6 +293,15 @@ echo ""
 # Ruby:
 echo "Installing Ruby..."
 sudo apt install ruby -y
+sudo apt install git curl autoconf bison build-essential libssl-dev libyaml-dev libreadline6-dev zlib1g-dev libncurses5-dev libffi-dev libgdbm6 libgdbm-dev libdb-dev -y
+curl -fsSL https://github.com/rbenv/rbenv-installer/raw/HEAD/bin/rbenv-installer | bash
+echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bashrc
+echo 'eval "$(rbenv init -)"' >> ~/.bashrc
+source ~/.bashrc
+echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.zshrc
+echo 'eval "$(rbenv init -)"' >> ~/.zshrc
+source ~/.zshrc
+rbenv -v
 echo "Ruby Installed!"
 echo ""
 
