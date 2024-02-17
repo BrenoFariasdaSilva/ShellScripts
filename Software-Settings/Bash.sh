0483906bb5b163fc996277b4d692a641bf080186
Breno Farias <brenofarias@Brenos-MacBook-Pro.local>
2024-02-17 10:43:21 -0300

    REFACTOR: Adding Echo Messages to the Software Installation Bash ShellScript

--- Software-Settings/Bash.sh
+++ Software-Settings/Bash.sh
@@ -4,6 +4,8 @@
 # chmod +x ./Bash.sh
 # sudo ./Bash.sh
 
+echo "Running the Bash ShellScript..."
+
 # Check if GNOME Terminal is installed
 if ! command -v gnome-terminal &> /dev/null; then
 	echo "GNOME Terminal is not installed on your system."
@@ -12,16 +14,32 @@ if ! command -v gnome-terminal &> /dev/null; then
 	echo "GNOME Terminal has been installed."
 fi
 
+echo "Setting the GNOME Terminal Keyboard Shortcuts..."
 # Define the custom keyboard shortcuts for GNOME Terminal
+
+echo "Open New Terminal Tab using 'Ctrl+T'"
 # Set the "New Tab" shortcut to "Ctrl+T"
 dconf write /org/gnome/terminal/legacy/keybindings/new-tab "'<Primary>t'" 
+
+echo "Open New Terminal Window using 'Ctrl+N'"
 # Set the "New Window" shortcut to "Ctrl+N"
 dconf write /org/gnome/terminal/legacy/keybindings/new-window "'<Primary>n'"
+
+echo "Close Terminal Tab using 'Ctrl+W'"
 # Set the "Close Tab" shortcut to "Ctrl+W"
 dconf write /org/gnome/terminal/legacy/keybindings/close-tab "'<Primary>w'"
+
+echo "Close Terminal Window using 'Alt+F4'"
 # Set the "Close Window" shortcut to "Alt+F4"
 dconf write /org/gnome/terminal/legacy/keybindings/close-window "'<Alt>F4'"
+
+echo "Copy Text using 'Ctrl+C'"
 # Set the "Copy" shortcut to "Ctrl+C"
 dconf write /org/gnome/terminal/legacy/keybindings/select-all "'<Primary>a'"
+
+echo "Paste Text using 'Ctrl+V'"
 # Set the "Paste" shortcut to "Ctrl+V"
 dconf write /org/gnome/terminal/legacy/keybindings/paste "'<Primary>v'"
+
+echo "Terminal Keyboard Shortcuts Set!"
+echo ""
