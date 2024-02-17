5d07bbd9313404e61e72ec086d3b8e246d9a653e
Breno Farias <brenofarias@Brenos-MacBook-Pro.local>
2024-02-17 10:47:00 -0300

    REFACTOR: Adding Echo Messages to the Software KeyBoardShortcut ShellScript

--- Software-Settings/KeyboardShortcuts.sh
+++ Software-Settings/KeyboardShortcuts.sh
@@ -4,24 +4,30 @@
 # chmod +x ./KeyboardShortcuts.sh
 # sudo ./KeyboardShortcuts.sh
 
-echo "Setting Keyboard Shortcuts..."
+echo "Setting Nautilus Keyboard Shortcuts..."
 
 # 1. Set the "Activities" Keyboard Shortcut to "Super/Windows"
+echo "Setting the 'Activities' Keyboard Shortcut to 'Super/Windows'..."
 gsettings set org.gnome.mutter overlay-key "'Super_L'"
 
 # 2. Set the "Show Applications" Keyboard Shortcut to "Super/Windows + S"
+echo "Setting the 'Show Applications' Keyboard Shortcut to 'Super/Windows + S'..."
 gsettings set org.gnome.shell.keybindings toggle-application-view "['<Super>s']"
 
 # 3. Set the "Show Desktop" Keyboard Shortcut to "Super/Windows + D"
+echo "Setting the 'Show Desktop' Keyboard Shortcut to 'Super/Windows + D'..."
 gsettings set org.gnome.desktop.wm.keybindings show-desktop "['<Super>d']"
 
 # 4. Set the "Home Folder" Keyboard Shortcut to "Super/Windows + E"
+echo "Setting the 'Home Folder' Keyboard Shortcut to 'Super/Windows + E'..."
 gsettings set org.gnome.settings-daemon.plugins.media-keys home "['<Super>e']"
 
 # 5. Set the "Terminal" Keyboard Shortcut to "Super/Windows + T"
+echo "Setting the 'Terminal' Keyboard Shortcut to 'Super/Windows + T'..."
 gsettings set org.gnome.settings-daemon.plugins.media-keys terminal "['<Super>t']"
 
 # 6. Set the "Close Window" Keyboard Shortcut to "Alt + F4"
+echo "Setting the 'Close Window' Keyboard Shortcut to 'Alt + F4'..."
 gsettings set org.gnome.desktop.wm.keybindings close "['<Alt>F4']"
 
 # If any of this doesn't work, add the failed ones manually:
@@ -30,3 +36,6 @@ gsettings set org.gnome.desktop.wm.keybindings close "['<Alt>F4']"
 # Name: AppName -> Example: Terminal
 # Command: gnome-AppCommand -> Example: gnome-terminal
 # Shortcut: Super/Windows + Key -> Example: Super + T
+
+echo "Nautilus Keyboard Shortcuts Set!"
+echo ""
