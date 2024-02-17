6feacbcb948eb25339c826c37134a4be8632ce0d
Breno Farias <brenofarias@Brenos-MacBook-Pro.local>
2024-02-17 10:45:02 -0300

    REFACTOR: Adding Echo Messages to the Software Installation DraculaTerminalTheme ShellScript

--- Software-Settings/DraculaTerminalTheme.sh
+++ Software-Settings/DraculaTerminalTheme.sh
@@ -4,6 +4,17 @@
 # chmod +x ./DraculaTerminalTheme.sh
 # sudo ./DraculaTerminalTheme.sh
 
+echo "Installing the Dracula Theme for GNOME Terminal..."
+
+# Check if GNOME Terminal is installed
+if ! command -v gnome-terminal &> /dev/null; then
+	echo "GNOME Terminal is not installed on your system."
+	echo "Installing GNOME Terminal..."
+	sudo apt install gnome-terminal -y
+	echo "GNOME Terminal has been installed."
+fi
+
+echo "Cloning the Dracula theme for GNOME Terminal..."
 # Clone the Dracula theme for GNOME Terminal
 git clone https://github.com/dracula/gnome-terminal.git ~/dracula-gnome-terminal
 
@@ -12,7 +23,9 @@ cd ~/dracula-gnome-terminal
 
 # Run the install script - it will prompt for confirmation to create a new profile
 # If you want to automate without creating a new profile, modifications are needed
+echo "Running the Dracula Theme Installation Script..."
 ./install.sh
+echo "Dracula theme has been installed for GNOME Terminal."
 
 # Instructions for setting Dracula as the default profile manually:
 # You would typically do this through the GNOME Terminal UI, or use gsettings as follows:
@@ -26,6 +39,9 @@ PROFILES=$(gsettings get org.gnome.Terminal.ProfilesList list)
 DRACULA_PROFILE_UUID=$(echo $PROFILES | tr -d "[]\', " | awk -F: '{print $(NF)}')
 
 # Set the Dracula profile as the default
+echo "Setting the Dracula Profile as the Default Profile..."
 gsettings set org.gnome.Terminal.ProfilesList default "$DRACULA_PROFILE_UUID"
+echo "Dracula Theme Set as the Default Profile!"
 
 echo "Dracula theme is installed and set as the default profile."
+echo ""
