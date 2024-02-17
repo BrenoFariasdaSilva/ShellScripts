4e44b0119778404b7498b41ff015001e3c6cf7dd
Breno Farias <brenofarias@Brenos-MacBook-Pro.local>
2024-02-17 11:05:01 -0300

    REFACTOR: Modifying the Grant Permissions ShellScript to handle the OS related ShellScripts

--- GrantPermissions.sh
+++ GrantPermissions.sh
@@ -6,10 +6,11 @@
 
 # Grant ShellScripts Execution Permissions
 grant_permissions() {
-  chmod +x Software-Installation/*.sh # Grant Execution Permission to all ShellScripts
-  chmod +x Software-Installation/SubScripts/*.sh # Grant Execution Permission to all ShellScripts inside SubScripts folder
-  chmod +x Software-Installation/SubScripts/Manual/*.sh # Grant Execution Permission to all ShellScripts inside SubScripts/Manual folder
-  chmod +x Software-Settings/*.sh # Grant Execution Permission to all ShellScripts
+  chmod +x Software-Installation/Linux/*.sh # Grant Execution Permission to the Linux ShellScripts
+  chmod +x Software-Installation/Linux/SubScripts/*.sh # Grant Execution Permission to all ShellScripts inside SubScripts folder
+  chmod +x Software-Installation/Linux/SubScripts/Manual/*.sh # Grant Execution Permission to all ShellScripts inside SubScripts/Manual folder
+  chmod +x Software-Installation/WSL/*.sh # Grant Execution Permission to the WSL ShellScripts
+  chmod +x Software-Settings/Linux/*.sh # Grant Execution Permission to all ShellScripts
 }
 
 # Run all functions
