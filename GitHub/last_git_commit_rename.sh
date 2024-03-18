3fd2ec3ba150eb28a9b5f4bb3594761d469e44eb
Breno Farias da Silva <breno.vicio78@gmail.com>
2024-03-18 10:01:18 -0300

    REFACTOR: Updating the Rename Last Commit on Git ShellScript Commands

--- GitHub/last_git_commit_rename.sh
+++ GitHub/last_git_commit_rename.sh
@@ -15,5 +15,6 @@ NEW_COMMIT_MESSAGE="$*"
 
 # Amending the last commit with the new commit message
 git commit --amend -m "$NEW_COMMIT_MESSAGE"
+git push --force origin main
 
 echo "The last commit message has been updated."
